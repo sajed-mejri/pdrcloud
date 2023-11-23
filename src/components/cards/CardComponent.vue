feb5c2c3a3576ce303f55e99a26dd1f584b8e1eb
sajed-mejri <sajed.mejri@esprit.tn>
2023-11-23 17:40:15 +0100

    chore(Backend): init laravel

---
+++ src/components/cards/CardComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+/* script */
+</script>
+
+<template>
+  <b-card title="title" img-src="url" img-alt="title" class="shadow mb-5">
+    <b-card-text>{{ photo.title }}</b-card-text>
+    <b-button> Add favorite </b-button>
+    <b-button> Remove favorite </b-button>
+  </b-card>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  background-color: whitesmoke;
+}
+</style>
