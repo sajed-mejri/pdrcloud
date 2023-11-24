9ee6692e781b8a3ac358b7a000990ea9a91378cd
sajed-mejri <sajed.mejri@esprit.tn>
2023-11-24 14:15:56 +0100

    style(global): make global scss

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createPinia } from "pinia";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-// import 'bootstrap-vue/src/icons.scss'
+import "./assets/global.scss";
 
 import App from "./App.vue";
 import router from "./router";
